d77c9a160bcc00a6d0a9cf50404c12dc294758f4
Tanmai Kota <[tanmai.kota@gmail.com](mailto:tanmai.kota@gmail.com)>
2026-02-18 10:19:37 +0530

    UC1/welcomwMessage

--- palindromecheckerapp.java
+++ palindromecheckerapp.java
@@ -1,3 +1,13 @@
+import java.lang.foreign.SymbolLookup;
+
 public class palindromecheckerapp {
+    public static void main(String[] args) {
+        System.out.println("welcome  to palindrome checker management system");
+        System.out.println(" version :1.0");
+        System.out.println("system initialized successfully");
+        
+        
+    }
     
+
 }
